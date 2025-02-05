7696a5e27b213f0d41c52ba23ae2a9de11bef479
samanta1401 <psamanta1401@gmail.com>
2025-02-05 11:55:59 +0530

    var

--- intro.cpp
+++ intro.cpp
@@ -1,6 +1,13 @@
-#include <iostream>
-using namespace std;
+// #include <iostream>
 
+// int main()
+// {
+//     std::cout << "Hello, World!" << std::endl;
+//     return 0;
+// }
+
+#include <iostream> //work with input and output objects
+using namespace std; // use names for objects and variables from the standard library.
 int main()
 {
     cout << "Hello World!";
